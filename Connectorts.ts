dd0aa746be0cf832b03edb814524582293739c74
SureshKumar1982Q <104483250+SureshKumar1982Q@users.noreply.github.com>
2022-04-27 10:52:32 +0530

    Add files via upload

---
+++ Connectorts.ts
@@ -0,0 +1,54 @@
+import { Client } from '@elastic/elasticsearch'
+
+const client = new Client({
+  cloud: { id: 'SureshKumar_ecommerce:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJGYxODQ1YzJlNTMxYjQ3Y2RhNGUzMjE1MWQwYmRhNzQ4JDJhM2RkYTIzZmE4MTRlNGNiYWMzNDMwMzk3YTEyZGU4' },
+  auth: { apiKey: 'base64EncodedKey' }
+})
+
+interface Document {
+  character: string
+  quote: string
+}
+
+async function run () {
+  // Let's start by indexing some data
+  await client.index({
+    index: 'game-of-thrones',
+    document: {
+      character: 'Ned Stark',
+      quote: 'Winter is coming.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    document: {
+      character: 'Daenerys Targaryen',
+      quote: 'I am the blood of the dragon.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    document: {
+      character: 'Tyrion Lannister',
+      quote: 'A mind needs books like a sword needs a whetstone.'
+    }
+  })
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: 'game-of-thrones' })
+
+  // Let's search!
+  const result= await client.search<Document>({
+    index: 'game-of-thrones',
+    query: {
+      match: { quote: 'winter' }
+    }
+  })
+
+  console.log(result.hits.hits)
+}
+
+run().catch(console.log)

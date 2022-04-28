61f06c8ea2b1e5b2e45ae66eb8320f8953a84186
SureshKumar1982Q <104483250+SureshKumar1982Q@users.noreply.github.com>
2022-04-28 11:07:26 +0530

    Add files via upload

--- Connector1.ts
+++ Connector1.ts
@@ -3,8 +3,8 @@ import { Client } from '@elastic/elasticsearch'
 const client = new Client({
   cloud: { id: 'SureshKumar_ecommerce:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJGYxODQ1YzJlNTMxYjQ3Y2RhNGUzMjE1MWQwYmRhNzQ4JDJhM2RkYTIzZmE4MTRlNGNiYWMzNDMwMzk3YTEyZGU4' },
   auth: {
-    username: 'suresh.nadar@neosoftmail.com',
-    password: 'Suresh231_'
+    username: 'elastic',
+    password: '169JKVpweEpxqrUpdB1vOu9N'
   }
 })
 
@@ -14,15 +14,37 @@ interface Document {
 }
 
 async function run () {
+  // Let's start by indexing some data
+  await client.index({
+    index: 'sureshkumar',
+    document: {
+      character: 'Good',
+      quote: 'Summer is okay.'
+    }
+  })
+  ///Adding second index data
+  await client.index({
+    index: 'sureshkumar',
+    document: {
+      character: 'AllisOkay',
+      quote: 'Next is winter'
+    }
+  })
+
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: 'sureshkumar' })
+
   // Let's search!
   const result= await client.search<Document>({
-    index: 'game-of-thrones',
+    index: 'sureshkumar',
     query: {
       match: { quote: 'winter' }
     }
   })
 
-  console.log("the hit is " +result.hits.hits)
+  console.log(result.hits.hits)
 }
 
 run().catch(console.log)
